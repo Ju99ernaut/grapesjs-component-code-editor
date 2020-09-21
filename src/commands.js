46f61245429ca3c6267891ec4f7453e4f3f29f47
Ju99ernaut <brendonzee@gmail.com>
2020-09-21 17:58:22 +0200

    Closes #3

--- src/commands.js
+++ src/commands.js
@@ -11,6 +11,7 @@ export default (editor, opts) => {
 
     cm.add(openCodeStr, {
         run: (editor, senderBtn) => {
+            // remove senderBtn from CodeEditor ref commit 5b341960bdcd7e4d93dc9d07000e87f7d9d875c1
             !codeEditor && (codeEditor = new CodeEditor(editor, opts)) && codeEditor.buildCodePanel();
             codeEditor.showCodePanel();
         },
