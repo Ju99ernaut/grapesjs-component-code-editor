8b7c9aa12e979e38d9ce32eded75c8b58af5158b
Ju99ernaut <brendonzee@gmail.com>
2020-11-22 15:35:53 +0200

    Update opts

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import commands from './commands';
 
 export default (editor, opts = {}) => {
-  const options = {
-    ...{
-      //Allow editing of javascript, set allowScripts to true for this to work
-      editJs: false,
-      //Remove component data eg data-gjs-type="..."
-      clearData: false,
-      //Code viewer options
-      codeViewOptions: {},
-      //Used to remove css from the Selector Manager
-      cleanCssBtn: true,
-      //Save HTML button text
-      htmlBtnText: 'Apply',
-      //Save CSS button text
-      cssBtnText: 'Apply',
-      //Clean CSS button text
-      cleanCssBtnText: 'Delete'
-    },
-    ...opts
-  };
+    const options = {
+        ...{
+            //Panel to append the code editor
+            panelId: 'views-container',
+            //Append to element instead of views-container
+            appendTo: '',
+            //State when open
+            openState: {
+                cv: '65%',
+                pn: '35%'
+            },
+            //State when closed
+            closedState: {
+                cv: '85%',
+                pn: '15%'
+            },
+            //Allow editing of javascript, set allowScripts to true for this to work
+            editJs: false,
+            //Remove component data eg data-gjs-type="..."
+            clearData: false,
+            //Code viewer options
+            codeViewOptions: {},
+            //Used to remove css from the Selector Manager
+            cleanCssBtn: true,
+            //Save HTML button text
+            htmlBtnText: 'Apply',
+            //Save CSS button text
+            cssBtnText: 'Apply',
+            //Clean CSS button text
+            cleanCssBtnText: 'Delete'
+        },
+        ...opts
+    };
 
-  // Load commands
-  commands(editor, options);
+    // Load commands
+    commands(editor, options);
 };
